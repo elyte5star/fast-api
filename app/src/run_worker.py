b06ce1954f2f7c4e77116c12968fcd9ce9f22486
elyte5star <checkuti@gmail.com>
2023-03-08 15:03:47 +0100

    dev

--- app/src/run_worker.py
+++ app/src/run_worker.py
@@ -1,11 +1,12 @@
-from resources.worker.base import Worker
+from resources.worker.base import WorkerBase
 from resources.worker.booking import BookingHandler
 from resources.settings.config import Settings
+from resources.schemas.misc.enums import WorkerType
 
 
 def run():
     config = Settings().from_toml_file()
-    worker = Worker(config)
+    worker = WorkerBase(config, WorkerType.Booking, config.queue_name[1])
     worker.booking_handler = BookingHandler(config)
     worker.run_forever()
 

25e0ff39bfbc3e69f7569348e9a8268724e96b76
elyte5star <checkuti@gmail.com>
2023-10-05 23:38:56 +0200

    dev

---
+++ deployment/common/generic_secrets.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+
+echo "Creating namespace and secrets!"
+
+kubectl apply -f ns.yml
+kubectl apply -f ./openssl/tls.yml
+kubectl apply -f regcred.yml
+kubectl create secret generic api-secrets --namespace demo --from-env-file=api_secrets.env
+kubectl create secret generic dashboard-secrets --namespace demo --from-env-file=ui_secrets.env
+
+echo "Deploying Database!"
+
+kubectl apply -f ../db/db_network.yml
+kubectl apply -f ../db/db_volume.yml
+kubectl apply -f ../db/db_deployment.yml
+
+sleep 20
+
+echo "Oops! I fell asleep for a 20 seconds!"
+
+echo "Deploying API!"
+
+kubectl apply -f ../api/api_network.yml
+kubectl apply -f ../api/api_deployment.yml
+
+
+sleep 20
+
+echo "Oops! I fell asleep for a 20 seconds!"
+
+echo "Deploying Frontend!"
+
+kubectl apply -f ../ui/ui_network.yml
+kubectl apply -f ../ui/ui_deployment.yml

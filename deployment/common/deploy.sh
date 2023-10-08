76e69d44f02366ec2637e44214c6436e1ce5a1bf
elyte5star <checkuti@gmail.com>
2023-10-08 22:57:47 +0200

    Fixed Queue Volume

--- deployment/common/deploy.sh
+++ deployment/common/deploy.sh
@@ -9,27 +9,27 @@ kubectl apply -f regcred.yml
 kubectl create secret generic api-secrets --namespace demo --from-env-file=api_secrets.env
 kubectl create secret generic dashboard-secrets --namespace demo --from-env-file=ui_secrets.env
 
-echo "Deploying Database!"
+echo "Deploying Database!...."
 
 kubectl apply -f ../db/db_network.yml
 kubectl apply -f ../db/db_volume.yml
 kubectl apply -f ../db/db_deployment.yml
 
-sleep 20
+sleep 30
 
-echo "Oops! I fell asleep for a 20 seconds!"
+echo "Oops! I fell asleep for a 30 seconds!"
 
-echo "Deploying API!"
+echo "Deploying API!...."
 
 kubectl apply -f ../api/api_network.yml
 kubectl apply -f ../api/api_deployment.yml
 
 
-sleep 20
+sleep 40
 
-echo "Oops! I fell asleep for a 20 seconds!"
+echo "Oops! I fell asleep for a 40 seconds!"
 
-echo "Deploying Frontend!"
+echo "Deploying Frontend!..."
 
 kubectl apply -f ../ui/ui_network.yml
 kubectl apply -f ../ui/ui_deployment.yml
